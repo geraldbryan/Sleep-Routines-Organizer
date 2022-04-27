98d9d056fecaaf4f3e40e827528526b4c61a0ca0
geraldbryan <geraldbryan9914@gmail.com>
2022-04-27 19:49:45 +0700

    Make the storyboard

--- SleepOrganizer/ViewController.swift
+++ SleepOrganizer/ViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordSleep: UIButton!
+    @IBOutlet weak var sleepRecommendation: UIButton!
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
